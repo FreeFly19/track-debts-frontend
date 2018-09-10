82bd58c2b47919e80a49cf717370240a1362ca6d
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-09-11 02:22:49 +0300

    Implement accept payment functionality

--- src/app/home/transaction/transaction-list-page/transaction-list-page.component.ts
+++ src/app/home/transaction/transaction-list-page/transaction-list-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Transaction} from '../../../core/transaction';
 import {HttpClient} from '@angular/common/http';
 import {Page} from '../../../core/page';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-list-page',
@@ -9,12 +10,32 @@ import {Page} from '../../../core/page';
 })
 export class TransactionListPageComponent implements OnInit {
   transactions?: Page<Transaction>;
+  users: User[] = [];
+  acceptMoneyForm: FormGroup;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
     this.http.get<Page<Transaction>>('/api/transactions')
       .subscribe(transactions => this.transactions = transactions);
+
+    this.http.get<User[]>('/api/users')
+      .subscribe(users => this.users = users);
+
+    this.acceptMoneyForm = this.fb.group({
+      'senderId': ['', Validators.compose([Validators.required, Validators.min(1)])],
+      'amount': ['', Validators.compose([Validators.required, Validators.min(1)])]
+    });
   }
 
+  loadPage(page) {
+    console.log(page);
+  }
+
+  acceptMoney() {
+    // TODO: not strong typed
+    this.http.post('/api/transactions', this.acceptMoneyForm.value)
+      .subscribe(() => this.ngOnInit());
+  }
 }

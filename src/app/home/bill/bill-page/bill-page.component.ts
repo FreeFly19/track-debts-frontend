68f49e25e1b20b7b0cbed7095e31552f5ebccabf
kolegran <kolegran@gmail.com>
2019-02-21 18:25:51 +0200

    Implemented create bill user functionality

--- src/app/home/bill/bill-page/bill-page.component.ts
+++ src/app/home/bill/bill-page/bill-page.component.ts
@@ -5,6 +5,7 @@ import {Bill} from '../../../core/bill/bill';
 import {AddBillItemCommand} from '../../../core/bill/add-bill-item-command';
 import {BillItem, BillItemParticipant} from '../../../core/bill/bill-item';
 import {UserService} from '../../../core/user.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-bill-page',
@@ -13,8 +14,11 @@ import {UserService} from '../../../core/user.service';
 })
 export class BillPageComponent implements OnInit {
   bill?: Bill;
+  users: User[] = [];
+  addBillUserForm: FormGroup;
 
   constructor(private http: HttpClient,
+              private fb: FormBuilder,
               private userService: UserService,
               private router: Router,
               private route: ActivatedRoute) {}
@@ -22,6 +26,13 @@ export class BillPageComponent implements OnInit {
   ngOnInit(): void {
     this.http.get<Bill>('/api/bills/' + this.route.snapshot.params['id'])
       .subscribe(bill => this.bill = bill);
+
+    this.http.get<User[]>('/api/users')
+      .subscribe(users => this.users = users);
+
+    this.addBillUserForm = this.fb.group({
+      'billUserId': ['', Validators.compose([Validators.required, Validators.min(1)])]
+    });
   }
 
   addItem(cmd: AddBillItemCommand) {
@@ -54,5 +65,10 @@ export class BillPageComponent implements OnInit {
 
   isCurrentUser(p: BillItemParticipant) {
     return p.user.id === this.userService.currentUser.id;
+  }
+
+  addBillUser() {
+    this.http.post('/api/bill/' + this.bill.id + '/users', this.addBillUserForm.value)
+      .subscribe(() => this.ngOnInit());
   }
 }
